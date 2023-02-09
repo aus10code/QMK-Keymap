afe162b1af9e9cd4cc5deee9dc5758a29005fc84
Austen Elam <aus10.code@gmail.com>
2023-02-09 10:35:19 -0600

    more updates to rgb layers

--- keymap.c
+++ keymap.c
@@ -121,6 +121,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     HSV hsv = {0, 255, 255};
 	uint8_t layer = get_highest_layer(layer_state);
+	RGB rgb = hsv_to_rgb(hsv);
 
 	for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
@@ -128,30 +129,47 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
                 if (index >= led_min && index < led_max && index != NO_LED &&
                 keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-					
 					if (layer_state_is(3)) {
-						//rgb_matrix_set_color(index, RGB_RED);
+						rgb_matrix_set_color(index, RGB_RED);
+						//hsv = (HSV) { (uint8_t)0, (uint8_t)100, (uint8_t)98 };
+						//rgb = hsv_to_rgb(hsv);
+						//rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
 					}
 					else if (layer_state_is(2)) {
-						hsv.h = 130;
-						//rgb_matrix_set_color(index, RGB_BLUE);
+						//hsv = (HSV) { (uint8_t)130, 100, 90 };
+						//rgb = hsv_to_rgb(hsv);
+						//rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+						rgb_matrix_set_color(index, RGB_BLUE);
 					}
 					else if (layer_state_is(1)) {
 						//(index, RGB_GREEN);
+						//hsv = (HSV) { (uint8_t)58, 100, 80 };
+						//rgb = hsv_to_rgb(hsv);
+						//rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+						rgb_matrix_set_color(index,  RGB_GREEN);
 					}
 					else {
 						//rgb_matrix_set_color(index, RGB_WHITE);
+						hsv = (HSV) { (uint8_t)180, 0, 100 };
+						rgb = hsv_to_rgb(hsv);
+						rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
 					}
-					
-					
+				}
+				else if (keymap_key_to_keycode(layer, (keypos_t){col,row}) <= KC_TRNS) {
+					hsv = (HSV) { (uint8_t)0, (uint8_t)0, (uint8_t)0 };
+					rgb = hsv_to_rgb(hsv);
+					rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
 				}
             }
         }
 
+	/*
     if (hsv.v > rgb_matrix_get_val()) {
         hsv.v = rgb_matrix_get_val();
     }
-    RGB rgb = hsv_to_rgb(hsv);
+	*/
+	
+    
 
     for (uint8_t i = led_min; i < led_max; i++) {
         if (HAS_FLAGS(g_led_config.flags[i], 0x01)) { // 0x01 == LED_FLAG_MODIFIER
@@ -210,14 +228,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
        KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_TRNS,   KC_P7,   KC_P8,   KC_P9, KC_RBRC, XXXXXXX,
+       RGB_MOD, _______, _______, _______, _______, _______,    KC_TRNS,   KC_P7,   KC_P8,   KC_P9, KC_RBRC, _______,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,   KC_P4,   KC_P5,   KC_P6, KC_PMNS, KC_PEQL,
+       RGB_TOG, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,    KC_PPLS,   KC_P4,   KC_P5,   KC_P6, KC_PMNS, KC_PEQL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-      RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR,  QK_BOOT,    KC_PAST,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,
+      RGB_RMOD, _______, _______, _______, EE_CLR,  QK_BOOT,    KC_PAST,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  XXXXXXX, XXXXXXX, KC_TRNS,    XXXXXXX, KC_TRNS,
-                                           XXXXXXX, XXXXXXX,      KC_P0
+                                  _______, _______, KC_TRNS,    _______, KC_TRNS,
+                                           _______, _______,      KC_P0
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -225,13 +243,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_BTN1, KC_BTN2, KC_WH_U, KC_WH_D, XXXXXXX, KC_VOLU,
+       KC_MNXT, _______, _______, _______, _______, _______,    KC_BTN1, KC_BTN2, KC_WH_U, KC_WH_D, , KC_VOLU,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPLY, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, KC_MUTE,
+       KC_MPLY, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, KC_MUTE,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPRV, XXXXXXX, XXXXXXX, XXXXXXX,  EE_CLR, QK_BOOT,    KC_NO,   KC_BTN1, KC_BTN2, KC_BTN3,   MO(2), KC_VOLD,
+       KC_MPRV, _______, _______, _______,  EE_CLR, QK_BOOT,    KC_NO,   KC_BTN1, KC_BTN2, KC_BTN3,   MO(2), KC_VOLD,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, _______,   KC_NO,    _______, XXXXXXX,
+                                  _______, _______,   KC_NO,    _______, _______,
                                            _______, _______,    _______
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
